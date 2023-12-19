f69bcd801e0c3fc02502cc7250c8eae3677c5539
kevinta893 <mumbler9486@gmail.com>
2023-12-19 21:48:02 +0900

    💄 Update main page navigation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,17 +6,27 @@
 </svelte:head>
 
 <main>
-	<h1>Advent of Svelte 2023 Solutions by Mumbler9486.</h1>
-	<p>
-		<a
-			href="https://github.com/mumbler9486/advent-of-svelte-2023"
-			target="_blank"
-			rel="noopener noreferrer">Source Code</a
-		>
-	</p>
-
-	<h1>Solutions</h1>
-	<a href="/day1">Day 1</a>
-	<a href="/day2">Day 2</a>
-	<a href="/day3">Day 3</a>
+	<div class="hero min-h-screen bg-base-200">
+		<div class="hero-content text-center">
+			<div class="max-w-md">
+				<h1 class="text-5xl font-bold">Advent of Svelte 2023</h1>
+				<div class="py-6">
+					<p>Solutions by Mumbler9486</p>
+					<p>
+						<a
+							href="https://github.com/mumbler9486/advent-of-svelte-2023"
+							target="_blank"
+							rel="noopener noreferrer">Source Code</a
+						>
+					</p>
+					<h1 class="text-3xl my-6">Solutions</h1>
+					<div class="flex flex-col gap-4">
+						<a href="/day1">Day 1</a>
+						<a href="/day2">Day 2</a>
+						<a href="/day3">Day 3</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </main>

4eceb02063f51ef797529aaefe4d23bc52b525f2
kevinta893 <mumbler9486@gmail.com>
2023-12-31 11:18:05 +0900

    💄 Add navigation to pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,5 +3,12 @@
 </script>
 
 <main>
-	<slot />
+	<div class="navbar absolute top-0 z-30 bg-base-100">
+		<a href="/" class="btn btn-ghost text-xl"
+			><img class="w-10" src="/favicon.png" alt="Home Icon" />Advent of Svelte 2023</a
+		>
+	</div>
+	<div class="bg-base-300 pt-16">
+		<slot />
+	</div>
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,49 +5,48 @@
 	<title>Advent of Svelte 2023</title>
 </svelte:head>
 
-<main>
-	<div class="hero min-h-screen bg-base-200">
-		<div class="hero-content text-center">
-			<div class="max-w-md">
-				<h1 class="text-5xl font-bold">Advent of Svelte 2023</h1>
-				<div class="py-6">
-					<p>Solutions by Mumbler9486</p>
-					<p>
-						<a
-							href="https://github.com/mumbler9486/advent-of-svelte-2023"
-							target="_blank"
-							rel="noopener noreferrer">Source Code</a
-						>
-					</p>
-					<h1 class="my-6 text-3xl">Solutions</h1>
-					<div class="flex flex-col gap-4">
-						<a href="/day1">✔ Day 1</a>
-						<a href="/day2">✔ Day 2</a>
-						<a href="/day3">✔ Day 3</a>
-						<a href="/day4">✔ Day 4</a>
-						<a href="/day5">✔ Day 5</a>
-						<a href="/day6">✔ Day 6</a>
-						<a href="/day7">✔ Day 7</a>
-						<a href="/day8">✔ Day 8</a>
-						<a href="/day9">✔ Day 9</a>
-						<a href="/day10">✔ Day 10</a>
-						<a href="/day11">✔ Day 11</a>
-						<a class="pointer-events-none" href="/day12">Day 12</a>
-						<a class="pointer-events-none" href="/day13">Day 13</a>
-						<a class="pointer-events-none" href="/day14">Day 14</a>
-						<a class="pointer-events-none" href="/day15">Day 15</a>
-						<a class="pointer-events-none" href="/day16">Day 16</a>
-						<a class="pointer-events-none" href="/day17">Day 17</a>
-						<a class="pointer-events-none" href="/day18">Day 18</a>
-						<a class="pointer-events-none" href="/day19">Day 19</a>
-						<a class="pointer-events-none" href="/day20">Day 20</a>
-						<a class="pointer-events-none" href="/day21">Day 21</a>
-						<a class="pointer-events-none" href="/day22">Day 22</a>
-						<a class="pointer-events-none" href="/day23">Day 23</a>
-						<a class="pointer-events-none" href="/day24">Day 24</a>
-					</div>
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content text-center">
+		<div class="max-w-md">
+			<h1 class="text-5xl font-bold">Advent of Svelte 2023</h1>
+			<div class="py-6">
+				<p>Solutions by Mumbler9486</p>
+				<p class="mt-4">
+					<a
+						class="btn btn-secondary"
+						href="https://github.com/mumbler9486/advent-of-svelte-2023"
+						target="_blank"
+						rel="noopener noreferrer">Source Code</a
+					>
+				</p>
+				<h1 class="my-6 text-3xl">Solutions</h1>
+				<div class="flex flex-col gap-4">
+					<a href="/day1">✔ Day 1</a>
+					<a href="/day2">✔ Day 2</a>
+					<a href="/day3">✔ Day 3</a>
+					<a href="/day4">✔ Day 4</a>
+					<a href="/day5">✔ Day 5</a>
+					<a href="/day6">✔ Day 6</a>
+					<a href="/day7">✔ Day 7</a>
+					<a href="/day8">✔ Day 8</a>
+					<a href="/day9">✔ Day 9</a>
+					<a href="/day10">✔ Day 10</a>
+					<a href="/day11">✔ Day 11</a>
+					<a class="pointer-events-none" href="/day12">Day 12</a>
+					<a class="pointer-events-none" href="/day13">Day 13</a>
+					<a class="pointer-events-none" href="/day14">Day 14</a>
+					<a class="pointer-events-none" href="/day15">Day 15</a>
+					<a class="pointer-events-none" href="/day16">Day 16</a>
+					<a class="pointer-events-none" href="/day17">Day 17</a>
+					<a class="pointer-events-none" href="/day18">Day 18</a>
+					<a class="pointer-events-none" href="/day19">Day 19</a>
+					<a class="pointer-events-none" href="/day20">Day 20</a>
+					<a class="pointer-events-none" href="/day21">Day 21</a>
+					<a class="pointer-events-none" href="/day22">Day 22</a>
+					<a class="pointer-events-none" href="/day23">Day 23</a>
+					<a class="pointer-events-none" href="/day24">Day 24</a>
 				</div>
 			</div>
 		</div>
 	</div>
-</main>
+</div>
